3a4b03f3b8a0a85d60fa9981d725a75a98124a48
codingmunk <tahmidshahaniislam@gmail.com>
2023-02-05 07:06:59 -0800

    Making server.py ready for testing with the android app. Still waiting for Nabil's survey for the idea.

--- server.py
+++ server.py
@@ -4,8 +4,13 @@ import uvicorn
 app = FastAPI()
 app.get('/Books')
 
-def Get_Book_Name():
-    return {}
+
+book_names = ['Physics paper 1' , 'Chemistry Paper 1' , 'Biology paper 1']
+dict_book_links = {}
+
+#Ready for testing!
+async def Get_Book_Name():
+    return book_names
 
 if __name__ == '__main__':
     uvicorn.run(app)
