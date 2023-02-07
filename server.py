6706d4ad16ff6fcb5fe132c43bcbff79d4d5a072
Tahmid Shahani Islam <124348116+codingmunk@users.noreply.github.com>
2023-02-07 10:09:14 +0000

    False code changed.

--- server.py
+++ server.py
@@ -2,15 +2,15 @@ from fastapi import FastAPI
 import uvicorn
 
 app = FastAPI()
-app.get('/Books')
-
 
 book_names = ['Physics paper 1' , 'Chemistry Paper 1' , 'Biology paper 1']
 dict_book_links = {}
 
-#Ready for testing!
-async def Get_Book_Name():
+
+@app.get('/Books')
+def Get_Book_Name():
     return book_names
 
+
 if __name__ == '__main__':
-    uvicorn.run(app)
+    uvicorn.run(app , host = '0.0.0.0' , port = 5000)
